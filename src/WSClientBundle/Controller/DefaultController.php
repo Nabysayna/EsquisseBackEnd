e3c74547032b5b7e0c4c0ddb79dbaf4760b34520
gningbamba <kha8224@gmail.com>
2017-06-02 12:10:36 +0000

    g

--- src/WSClientBundle/Controller/DefaultController.php
+++ src/WSClientBundle/Controller/DefaultController.php
@@ -19,7 +19,7 @@ class DefaultController extends Controller
 
     public function loggingAction()
     {
-        $client = new \nusoap_client('http://localhost/EsquisseBackEnd/web/app_dev.php/invest/logging?wsdl', true);
+        $client = new \nusoap_client('http://localhost:8888/EsquisseBackEnd/web/app_dev.php/invest/logging?wsdl', true);
         $user = array('login' => 'assane.ka@bbstvnet.com', 'pwd' => 'assane');
         $result = $client->call('authentification', array('user' => $user));
 
@@ -28,9 +28,9 @@ class DefaultController extends Controller
 
     public function disconnectAction()
     {
-        $client = new \nusoap_client('http://localhost/EsquisseBackEnd/web/app_dev.php/invest/logging?wsdl', true);
+        $client = new \nusoap_client('http://localhost:8888/EsquisseBackEnd/web/app_dev.php/invest/logging?wsdl', true);
         $fromUs =  sha1("bay3k00_f1_n10un") ;
-        $user = array( 'token' => "4d7cf1e43dd42cf993af52d55bfa0ff14e37b050", 'hdeconnexion' => "459d" ) ;
+        $user = array( 'token' => "8a6e82beca15168315400832ec3bc0f318a20623", 'hdeconnexion' => "459d" ) ;
         $result = $client->call('deconnexion', array('user' => $user));
 
         return new JsonResponse(array('result' => $result));  /* assane.ka@bbstvnet.com */
