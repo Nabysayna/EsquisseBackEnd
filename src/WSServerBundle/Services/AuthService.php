26cb0880c2e9053802b9ff05a6e7abb36a3cb1c9
Nabysayna <nabysayna@gmail.com>
2017-06-29 10:31:07 +0000

    auth by mailing

--- src/WSServerBundle/Services/AuthService.php
+++ src/WSServerBundle/Services/AuthService.php
@@ -3,13 +3,15 @@
 namespace WSServerBundle\Services;
 
 use WSServerBundle\Entity\Authorizedsessions;
+use WSServerBundle\Entity\Tokentemporaire;
 
 class AuthService
 {
     private $em;
-
-  	public function __construct(\Doctrine\ORM\EntityManager $entityManager){
+    private $mailer;
+  	public function __construct(\Doctrine\ORM\EntityManager $entityManager, $mailer){
   		$this->em = $entityManager;
+      $this->mailer = $mailer;
   	}
 
     public function testDB(){
@@ -23,7 +25,7 @@ class AuthService
         return 'OKA says hello , '.$name;
     }
 
-    function authentification($user) {
+    function  authentification($user) {
       
       $dbuser = $this->em->getRepository('WSServerBundle:Users')->findOneBy(array('login' => $user->login, 'pwd' => $user->pwd));
 
@@ -33,6 +35,34 @@ class AuthService
       }
       else {
 
+          $sessionStart = new \Datetime() ;
+          $tokenTemp =  microtime() ;
+          $newTempRow = new Tokentemporaire();
+          $newTempRow->setIdUser($dbuser->getIdUser());
+          $newTempRow->setAccessLevel($dbuser->getAccesslevel());
+          $newTempRow->setTokentemporaire($tokenTemp);
+          $newTempRow->setCountingpoint($sessionStart);
+
+          $this->em->persist($newTempRow);
+          $this->em->flush();
+
+          $this->envoyerSMS('naby.hikam@gmail.com', $dbuser->getLogin(), strval($tokenTemp) ) ;
+
+          return 'true';
+      }
+    }
+
+    function authentificationPhaseTwo($params)
+    {
+      $tempRow = $this->em->getRepository('WSServerBundle:Tokentemporaire')->findOneBy(array('tokentemporaire'=>$params->tokentemporaire));
+
+      $dbuser = $this->em->getRepository('WSServerBundle:Users')->findOneBy(array('idUser' => $tempRow->getIdUser()));
+
+      if (empty($dbuser)) {
+        $reponse = array('prenom' => '', 'token' => '', 'reponse' => false);
+        return ''. json_encode($reponse);
+      }
+      else {
         if ( $dbuser->getDependsOn()==0 )
           $dbprerogative = $this->em->getRepository('WSServerBundle:Prerogatives')->findOneBy(array('idUser'=>$dbuser->getIdUser())) ;
         else
@@ -51,9 +81,10 @@ class AuthService
           $authorizedsession->setDependsOn($dbuser->getDependsOn());
           $authorizedsession->setSessionStart($sessionStart);
 
+          $this->em->remove($tempRow) ;
           $this->em->persist($authorizedsession);
           $this->em->flush();
-         
+
           $reponse = array(
             'prenom' => $dbuser->getPrenom(),
             'accessLevel' => $dbuser->getAccesslevel(),
@@ -62,8 +93,9 @@ class AuthService
             'reponse' => true
           );          
           return ''. json_encode($reponse);
-      }
+        }
     }
+
 
     function deconnexion($user)
     {
@@ -74,9 +106,19 @@ class AuthService
         return 1;    
       }catch(Exception $e){
         return 0;
-      }
-    
+      }    
     }
 
+
+    function envoyerSMS($from, $to, $content)
+    {
+        $message = \Swift_Message::newInstance() ;
+        $message->setContentType('text/html') ;
+        $message->setSubject('Authentification WELMA') ;
+        $message->setFrom($from) ;
+        $message->setTo($to) ;
+        $message->setBody($content);
+        $this->mailer->send($message);
+    }
     
 }
