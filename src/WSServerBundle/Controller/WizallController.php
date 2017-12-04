7dc5b4ff5876f92a3d594f5b2c2a73aa533f9939
okako <kasanesn@hotmail.com>
2017-12-04 11:37:35 +0000

    Prepar Wizall

---
+++ src/WSServerBundle/Controller/WizallController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WSServerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class WizallController extends Controller
+{
+    public function __construct() {  
+        header("Access-Control-Allow-Origin: *"); 
+        header("Access-Control-Allow-Headers: SOAPAction, Content-Type"); 
+    }
+
+    public function wizallAction()
+    {
+        $server = new \SoapServer('wsdl_wizall/ws_wizall.wsdl',array('cache_wsdl' => WSDL_CACHE_NONE));
+        $server->setObject($this->get('test_server_wizall'));
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'text/xml; charset=ISO-8859-1');
+
+        ob_start();
+        $server->handle();
+        $response->setContent(ob_get_clean());
+
+        return $response;
+    }
+
+}
---
+++ src/WSServerBundle/Services/WizallService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace WSServerBundle\Services;
+
+
+class WizallService
+{
+
+
+  public function __construct()
+  {
+  }
+
+  function intouchCashin($params)
+  {
+    return ''. json_encode( array('errorCode' => 1, 'response' => $params) );
+  }
+
+
+  
+
+}
+
